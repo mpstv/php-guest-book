31a16fc194a01bc93ecf5c4dfb910139859b5b5c
mpstv <mpstv@outlook.com>
2020-03-08 15:31:50 +0300

    add text-area insted input on review create page

--- App/Views/Review/index.php
+++ App/Views/Review/index.php
@@ -2,7 +2,7 @@
 <a href="/">Back to list</a>
 <form action="create" method="post">
  <p>Your name: <input type="text" name="author" /></p>
- <p>Review: <input type="text" name="content" /></p>
+ <p>Review: <textarea type="text" name="content" ></textarea></p>
  <p><input type="submit" value="Create" /></p>
 </form>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '\\App\\Views\\Shared\\footer.php'?>
