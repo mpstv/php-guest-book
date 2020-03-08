1c58dc72a3bea4b3c35a45b3ed23cbc2e76e8aa9
mpstv <mpstv@outlook.com>
2020-03-08 20:34:43 +0300

    adding some styles

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -1,7 +1,9 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '\\App\\Views\\Shared\\header.php'?>
 <a href="Review/index">Create review</a>
 <?php foreach ($viewData as $review): ?>
-    <h3><?=$review->author?> #<?=$review->id?></h3>
-    <p><?=$review->content?></p>
+    <div class="review">
+        <h3><?=$review->author?> #<?=$review->id?></h3>
+        <p><?=$review->content?></p>
+    </div>
 <?php endforeach;?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '\\App\\Views\\Shared\\footer.php'?>
--- App/Views/Shared/header.php
+++ App/Views/Shared/header.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Guest book</title>
+    <link rel="stylesheet" href="<?=$SERVER['DOCUMENT_ROOT'] . '/App/Assets/styles.css'?>">
 </head>
 <body>
-<h1> Guest book </h1>
+<div class="main-content">
+<h1 class="main-header"> Guest book </h1>
